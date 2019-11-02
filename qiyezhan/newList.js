03954e8abf498836bdf1631a26b7beefeed8bb37
yyj <380120971@qq.com>
2019-11-02 14:26:35 +0800

    企业站完成一版 2019/11/02/14：26 PM

---
+++ qiyezhan/newList.js
@@ -0,0 +1,44 @@
+axios.get('http://mock-api.com/5n9682Ko.mock/news-list_page1')
+    .then(function(res) {
+        $(".loading i").css("animation-play-state","running");
+        for (var i = 0; i < res.data.length; i++) {
+            $('.nl').append("<li><a href='newMain.html?ind="+res.data[i].ind+"'><img src=" + res.data[i].url + "><div class='ant'><h4>" + res.data[i].title + "</h4><span class='date'>" + res.data[i].date + "</span><p>" + res.data[i].ant + "</p></div></a></li>");
+        }
+        $(".loading").css("display","none");
+        $(".loading i").css("animation-play-state","paused");
+        $('#page1').addClass("on");
+    }).catch(function(res) {})
+$(document).ready(function() {
+    /*获取第一页数据*/ 
+    $('#page1').click(function() {
+        $('.nl li').remove();
+        $(".loading").css("display","block");
+        $(".loading i").css("animation-play-state","running");
+        axios.get('http://mock-api.com/5n9682Ko.mock/news-list_page1')
+            .then(function(res) {            
+                for (var i = 0; i < res.data.length; i++) {
+                    $('.nl').append("<li><a href='newMain.html?ind="+res.data[i].ind+"'><img src=" + res.data[i].url + "><div class='ant'><h4>" + res.data[i].title + "</h4><span class='date'>" + res.data[i].date + "</span><p>" + res.data[i].ant + "</p></div></a></li>");
+                }
+                $(".loading").css("display","none");
+                $(".loading i").css("animation-play-state","paused");
+            }).catch(function(res) { console.log('error', res); })
+        $('#page2').removeClass("on");
+        $('#page1').addClass("on");
+    })
+    /*获取第二页数据*/
+    $('#page2').click(function() {
+        $('.nl li').remove();
+        $(".loading").css("display","block");
+        $(".loading i").css("animation-play-state","running");
+        axios.get('http://mock-api.com/5n9682Ko.mock/news-list_page2')
+            .then(function(res) {
+                for (var i = 0; i < res.data.length; i++) {
+                    $('.nl').append("<li><a href='newMain.html?ind="+res.data[i].ind+"'><img src=" + res.data[i].url + "><div class='ant'><h4>" + res.data[i].title + "</h4><span class='date'>" + res.data[i].date + "</span><p>" + res.data[i].ant + "</p></div></a></li>");
+                }
+                $(".loading").css("display","none");
+                $(".loading i").css("animation-play-state","paused");
+            }).catch(function(res) { console.log('error', res); })
+        $('#pagelist #page1').removeClass("on");
+        $('#page2').addClass("on");
+    })
+})
---
+++ qiyezhan/newMain.js
@@ -0,0 +1,21 @@
+function getQueryString(name) {
+    
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+var i=getQueryString("ind")
+$(document).ready(function() {
+    /**启用动画 */
+    $(".loading i").css("animation-play-state","running");
+    axios.get('http://mock-api.com/5n9682Ko.mock/newsMain')
+            .then(function(res) {
+                console.log(res.data[i]);
+                    $('title').html(res.data[i].title);
+                    $('#content').append("<div class='content_top'><h2>"+res.data[i].title+"</h2><span>发布日期:" + res.data[i].date + "</span></div><div class='content_bottom'><img src="
+                    +res.data[i].url+"></div><p class='con'>"+res.data[i].ant+"</p>"
+                       );
+                    $(".loading").css("display","none");
+                    $(".loading i").css("animation-play-state","paused");
+            }).catch(function(res) { console.log('error', res); })
+        })
